076de7cc0fac4a59767e5362b8664e2d34a6361c
samuel <samuechen@cs.stonybrook.edu>
2019-04-15 21:21:25 -0400

    collision

--- main/js/main.js
+++ main/js/main.js
@@ -343,8 +343,8 @@ var InGame = new Phaser.Class({
         const tileset = level1.addTilesetImage('tileset');
         const objects = level1.addTilesetImage('agentsprite', 'objects');
         //make the layer(s) from tileset
-         this.backgroundLayer = level1.createStaticLayer('backgroundLayer',tileset);
-        const blockedLayer = level1.createStaticLayer('blockedLayer',tileset);
+        const backgroundLayer = level1.createStaticLayer('backgroundLayer',tileset);
+        this.blockedLayer = level1.createStaticLayer('blockedLayer',tileset);
         const trapsLayer = level1.createDynamicLayer('trapsLayer',objects);
         //set collision of blocked layer
         //blockedLayer.setCollisionByProperty({collides: true});
@@ -375,28 +375,28 @@ var InGame = new Phaser.Class({
             switch (event.keyCode)
             {
                 case 37:
-                if (this.blockedLayer.getTileAt(this.player.x - 64 + camera.x, this.player.y + camera.y) === null)
+                if (this.blockedLayer.getTileAtWorldXY(this.player.x - 64, this.player.y) === null)
                 {
                 this.player.x -= 64;
                 }
                 break;
 
                 case 38:
-                if (this.blockedLayer.getTileAt(this.player.x+ camera.x, this.player.y - 64 + camera.y) === null)
+                if (this.blockedLayer.getTileAtWorldXY(this.player.x, this.player.y - 64) === null)
                 {
                 this.player.y -= 64;
                 }
                 break;
 
                 case 39:
-                if (this.blockedLayer.getTileAt(this.player.x + 64 + camera.x, this.player.y + camera.y) === null)
+                if (this.blockedLayer.getTileAtWorldXY(this.player.x + 64 , this.player.y) === null)
                 {
                 this.player.x += 64;
                 }
                 break;
 
                 case 40:
-                if (this.blockedLayer.getTileAt(this.player.x+ camera.x, this.player.y + 64 + camera.y) === null)
+                if (this.blockedLayer.getTileAtWorldXY(this.player.x, this.player.y + 64) === null)
                 {
                 this.player.y += 64;
                 }
